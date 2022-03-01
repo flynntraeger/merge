85cd58d065559e5a55b42fe5fa6d0b1b8bfea1a7
ember fu <71354928+emberka@users.noreply.github.com>
2022-02-28 17:30:05 -0800

    Update App.js

--- merge/App.js
+++ merge/App.js
@@ -4,6 +4,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
 
+const Stack = createStackNavigator();
+
 export default function App() {
     return (
         <NavigationContainer>
