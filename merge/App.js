0112d89e268336448843eee3dd3234b3e4505228
ember fu <71354928+emberka@users.noreply.github.com>
2022-02-28 17:14:59 -0800

    page nav setup 1
    screens folder, empty initial home screen set up example, app.js using stack navigation

--- merge/App.js
+++ merge/App.js
@@ -1,38 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import TopBar from './components/TopBar/TopBar';
-import NavBar from './components/NavBar/NavBar';
-import DirectMessage from './components/DirectMessage/DirectMessage';
-import GameInvite from './components/GameInvite/GameInvite';
-import GameCard from './components/GameCard/GameCard';
-import UserCard from './components/UserCard/UserCard';
+import { StyleSheet } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen/HomeScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <TopBar title={"My Feed"} desc={"View your latest notifications"}/>
-      <Text>Miranda was here!</Text>
-      <DirectMessage 
-        username="Naftyy"
-        timestamp="Feb 8 @ 9:20PM"
-        message="Get on Valorant bark bark bark grrrrr bark"
-        picURL="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/103/original/Killua_2.jpg?1621356497"
-      />
-      <GameInvite username="Naftyy" game="Valorant" time="6pm" date="2/8" active={true}/>
-      <GameCard
-        game="Minecraft"
-        picURL="https://image.api.playstation.com/vulcan/img/cfn/11307uYG0CXzRuA9aryByTHYrQLFz-HVQ3VVl7aAysxK15HMpqjkAIcC_R5vdfZt52hAXQNHoYhSuoSq_46_MT_tDBcLu49I.png"
-      />
-      <UserCard
-        username="Naftyy"
-        level="17"
-        progress={0.25}
-        picURL="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/103/original/Killua_2.jpg?1621356497"
-      />
-      <NavBar/>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home Screen" component={HomeScreen} options={{headerShown: false}}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
---
+++ merge/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import TopBar from '../components/TopBar/TopBar';
+import NavBar from '../components/NavBar/NavBar';
+import DirectMessage from '../components/DirectMessage/DirectMessage';
+import GameInvite from '../components/GameInvite/GameInvite';
+import GameCard from '../components/GameCard/GameCard';
+import UserCard from '../components/UserCard/UserCard';
+
+export default function HomeScreen({ route, navigation}) {
+	const params = route.params;
+	//home screen code blah blah
+}
