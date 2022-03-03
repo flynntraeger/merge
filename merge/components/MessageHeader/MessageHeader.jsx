29b492566f16f8f2a9dd64b61c80b26003f744f6
MirandaDiaz <62873184+MirandaDiaz@users.noreply.github.com>
2022-03-02 17:39:55 -0800

    MessageHeader Component Created

---
+++ merge/components/MessageHeader/MessageHeader.jsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import { Text, View, StyleSheet} from 'react-native';
+import { Avatar, Button } from 'react-native-elements';
+import { Badge } from 'react-native-paper';
+import { ChevronRight} from "react-native-feather";
+
+class MessageHeader extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.leftCol}>
+                <Avatar 
+                    size={50}
+                    rounded
+                    source={{uri: this.props.picURL}}
+                />
+                <Badge style={styles.badge}>
+                    {this.props.value}
+                </Badge>
+            </View>
+            <View style={styles.rightCol}>
+                <View style={styles.topRow}>
+                    <Text style={styles.nameText}>{this.props.username}</Text>
+                    <View style={styles.timestampRow}>
+                        <Text style={styles.timestampText}>{this.props.timestamp}</Text>
+                        <Button 
+                            icon={<ChevronRight stroke="#888888" width={15} height={15} />}
+                            buttonStyle={styles.iconButton}
+                        />
+                    </View>
+                </View>
+                <Text style={styles.messageText}>{this.props.message}</Text>
+            </View>
+        </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+      width: "95%",
+      borderRadius: 10,
+      borderBottomWidth: 1,
+      borderColor: "#444444",
+      flexWrap: "wrap",
+      flexDirection:"row",
+      justifyContent: "flex-start",
+      padding: 15,
+      fontFamily: 'Manrope'
+    },
+    leftCol: {
+      flexDirection:"row",
+      justifyContent:"flex-start"
+    },
+    rightCol: {
+      flex:1,
+      flexDirection:"column",
+      paddingLeft:15
+    },
+    topRow: {
+        flexDirection:"row",
+        justifyContent:"space-between",
+        paddingBottom: 2
+    },
+    timestampRow: {
+        flexDirection:"row",
+        justifyContent:"space-between",
+        alignItems: "center"
+    },
+    nameText: {
+        fontSize: 14,
+        color: "#FFFFFF",
+        fontWeight: "bold",
+        textAlign: 'left',
+    },
+    timestampText: {
+      fontSize: 11,
+      textAlign: 'right',
+      color: "#888888",
+    },
+    messageText: {
+        fontSize: 13,
+        color: "#DDDDDD",
+        fontWeight: "100",
+        textAlign: 'left'
+    },
+    badge: {
+        position: 'absolute',
+        top: 0,
+        left: 35,
+        backgroundColor: "#57B288",
+        borderWidth: 1,
+        borderColor: "#353535",
+        color: "#222222"
+    },
+    iconButton: {
+        backgroundColor: "#222222",
+        padding: 0
+    }
+  });
+
+export default MessageHeader;
--- merge/screens/DMsScreen.js
+++ merge/screens/DMsScreen.js
@@ -1,18 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import TopBar from '../components/TopBar/TopBar';
-import DirectMessage from '../components/DirectMessage/DirectMessage';
-import GameInvite from '../components/GameInvite/GameInvite';
-import GameCard from '../components/GameCard/GameCard';
-import UserCard from '../components/UserCard/UserCard';
+import MessageHeader from '../components/MessageHeader/MessageHeader';
+import { SearchBar } from 'react-native-elements';
+import { Divider } from 'react-native-paper';
 
 export default function DMsScreen({ route, navigation}) {
     const params = route.params;
-	//notif screen code blah blah
+    const [searchText, setSearchText] = useState("hsafbhjasbfjbdhjs");
+
     return (
         <View style={styles.container}>
           <TopBar title="My Messages" desc="Chat with your bonds"/>
-            <Text> Testing DMs Screen </Text>
+            <SearchBar
+              containerStyle={styles.searchBarContainer}
+              inputContainerStyle={styles.searchBarTextArea}
+              placeholder="Search by username..."
+              onChangeText={(value)=>setSearchText(value)}
+              value={searchText}
+              inputStyle={styles.searchBarText}
+              searchIcon = {{color: "#888888"}}
+              clearIcon = {{color: "#888888"}}
+              placeholderTextColor = {"#888888"}
+            />
+            <MessageHeader 
+              username={"Naftyy"}
+              timestamp={"10:00 AM "}
+              value={10}
+              message={"ajhbfj ajhbfdjh jhafbdfjh am aljndf al,jdfn"}
+              picURL={"https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/103/original/Killua_2.jpg?1621356497"}
+            />
+            <MessageHeader 
+              username={"Naftyy"}
+              timestamp={"10:00 AM "}
+              value={10}
+              message={"ajhbfj ajhbfdjh jhafbdfjh am aljndf al,jdfn"}
+              picURL={"https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/103/original/Killua_2.jpg?1621356497"}
+            />
+            <MessageHeader 
+              username={"Naftyy"}
+              timestamp={"10:00 AM "}
+              value={10}
+              message={"ajhbfj ajhbfdjh jhafbdfjh am aljndf al,jdfn"}
+              picURL={"https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/103/original/Killua_2.jpg?1621356497"}
+            />
+            
         </View>
     );
 }
@@ -20,7 +52,21 @@ export default function DMsScreen({ route, navigation}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 95,
+    backgroundColor: "#222222",
     alignItems: 'center'
   },
+  searchBarContainer: {
+    width:"95%",
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    backgroundColor: "#222222"
+  },
+  searchBarTextArea: {
+    backgroundColor: "#1D1D1D"
+  },
+  searchBarText: {
+    color:"#888888"
+  }
 });
