a6a6f3c1addec6e1893a349fdb13f8c600b54b38
ember fu <71354928+emberka@users.noreply.github.com>
2022-03-08 20:07:20 -0800

    ScrollView + hardcoded events

--- merge/screens/EventsScreen.js
+++ merge/screens/EventsScreen.js
@@ -16,16 +16,22 @@ export default function EventsScreen({ route, navigation}) {
     return (
         <View style={styles.container}>
           <TopBar title="My Events" desc="View your gaming schedule"/>
-          <CreateEventButton />
-          <DividerWithText text="Upcoming Events" />
-          <ListedEvent />
-          <ListedEvent />
-          <DividerWithText text="Current Events" />
-          <ListedEvent />
-          <ListedEvent />
-          <DividerWithText text="Previous Events" />
-          <ListedEvent />
-          <ListedEvent />
+          <ScrollView style={styles.scroll}>
+              <View style={styles.body}>
+                  <CreateEventButton />
+                  <DividerWithText text="Upcoming Events" />
+                  <ListedEvent eventTitle="Among Us, Jackbox, more!!!" month="Feb" day="18" time="6:00PM" numInvites="8" topGame="Among Us"/>
+                  <ListedEvent eventTitle="Vibe and Play" month="Feb" day="21" time="7:30PM" numInvites="5" topGame="JackBox"/>
+                  <DividerWithText text="Current Events" />
+                  <ListedEvent eventTitle="Dubway's Day to Choose" month="Feb" day="14" time="3:00PM" numInvites="3" topGame="GangBeasts"/>
+                  <DividerWithText text="Previous Events" />
+                  <ListedEvent eventTitle="Torture Session" month="Jan" day="24" time="6:00PM" numInvites="4" topGame="LoL"/>
+                  <ListedEvent eventTitle="Gaming w/ da homies" month="Jan" day="5" time="3:00PM" numInvites="5" topGame="Valorant"/>
+                  <ListedEvent eventTitle="Merry Chrysler" month="Dec" day="26" time="8:00PM" numInvites="12" topGame="Fall Guys"/>
+                  <ListedEvent eventTitle="Raiding Party" month="Dec" day="12" time="2:00PM" numInvites="4" topGame="Monster Hunt"/>
+                  <ListedEvent eventTitle="Squad Up" month="Nov" day="29" time="7:00PM" numInvites="3" topGame="Apex Legends"/>
+              </View>
+          </ ScrollView>
         </View>
     );
 }
@@ -36,5 +42,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#222222",
     justifyContent: 'flex-start',
     alignItems: 'center',
+  },
+  scroll: {
+    width: '100%'
+  },
+  body: {
+    width:"100%",
+    alignItems: "center",
+    marginTop: 0,
+    marginBottom: 370
   },
 });
