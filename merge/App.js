daece23a5ebf0126b94b64fb9def047c7980208d
Andrew Li <li.jj.andrew@gmail.com>
2022-03-09 10:52:39 -0800

    add starter screen toggle for dev

--- merge/App.js
+++ merge/App.js
@@ -1,5 +1,4 @@
 import { StyleSheet } from 'react-native';
-import TopBar from './components/TopBar/TopBar';
 import NavBar from './components/NavBar/NavBar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
@@ -16,6 +15,8 @@ import Start2Screen from './screens/Start2Screen';
 import Start3Screen from './screens/Start3Screen';
 import CreateEventScreen from './screens/CreateEventScreen';
 
+const SHOW_START_SCREENS = false;
+
 const Stack = createStackNavigator();
 
 export default class App extends React.Component {
@@ -23,13 +24,11 @@ export default class App extends React.Component {
         super(props);
         this.state = {
             username: null,
-            showNav: true,
-            // showNav: true,
+            showNav: !SHOW_START_SCREENS,
         };
     }
 
     setUsername = (username) => {
-        console.log(`username set to ${username}`);
         this.setState({ username });
     }
 
@@ -41,8 +40,14 @@ export default class App extends React.Component {
         return (
             <NavigationContainer>
                 <Stack.Navigator screenOptions={{ animationEnabled: false }}>
-                    {/*<Stack.Screen name="Start2 Screen" component={Start2Screen} options={{ headerShown: false }} />
-                    <Stack.Screen name="Start3 Screen" component={Start3Screen} options={{ headerShown: false }} initialParams={{ setShowNav: this.setShowNav }} /> */}
+                    {
+                        SHOW_START_SCREENS ?
+                            (
+                                [<Stack.Screen name="Start1 Screen" component={Start1Screen} options={{ headerShown: false }} initialParams={{ setUsername: this.setUsername }} />,
+                                <Stack.Screen name="Start2 Screen" component={Start2Screen} options={{ headerShown: false }} />,
+                                <Stack.Screen name="Start3 Screen" component={Start3Screen} options={{ headerShown: false }} initialParams={{ setShowNav: this.setShowNav }} />]
+                            ) : null
+                    }
                     <Stack.Screen name="Home Screen" component={HomeScreen} options={{ headerShown: false }} />
                     <Stack.Screen name="Notif Screen" component={NotifScreen} options={{ headerShown: false }} />
                     <Stack.Screen name="DMs Screen" component={DMsScreen} options={{ headerShown: false }} />
