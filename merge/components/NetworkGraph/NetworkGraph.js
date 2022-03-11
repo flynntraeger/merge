618045182f86c049d31398f03fd14e03af678ffd
ember fu <71354928+emberka@users.noreply.github.com>
2022-03-10 21:36:52 -0800

    revert

--- merge/components/NetworkGraph/NetworkGraph.js
+++ merge/components/NetworkGraph/NetworkGraph.js
@@ -100,9 +100,9 @@ class NetworkGraph extends Component {
         circleStrokeColor = centralCircleFillColor
       return (
         <G key={"circle" + index} onPress={this.circleClicked(index)}>
-          {/* <Circle cx={circleCoOrds.xCordinate} cy={circleCoOrds.yCordinate} r={otherCirclesRadius} stroke={circleStrokeColor} strokeWidth="2" fill={otherCircleFillColor} /> */}
-          {/* <circle cx={circleCoOrds.xCordinate} cy={circleCoOrds.yCordinate} r={otherCirclesRadius} stroke={circleStrokeColor} strokeWidth="2" fill="none" /> */}
-          <image xlinkHref={profiles[title].imgurl} x={circleCoOrds.xCordinate - otherCirclesRadius} y={circleCoOrds.yCordinate - otherCirclesRadius} height={2 * otherCirclesRadius} width={2 * otherCirclesRadius} />
+          { <Circle cx={circleCoOrds.xCordinate} cy={circleCoOrds.yCordinate} r={otherCirclesRadius} stroke={circleStrokeColor} strokeWidth="2" fill={otherCircleFillColor} /> }
+          { <Circle cx={circleCoOrds.xCordinate} cy={circleCoOrds.yCordinate} r={otherCirclesRadius} stroke={circleStrokeColor} strokeWidth="2" fill="none" /> }
+          {/*<Image xlinkHref={profiles[title].imgurl} x={circleCoOrds.xCordinate - otherCirclesRadius} y={circleCoOrds.yCordinate - otherCirclesRadius} height={2 * otherCirclesRadius} width={2 * otherCirclesRadius} />*/}
           <Text x={circleCoOrds.xCordinate - otherCirclesRadius + 15} y={circleCoOrds.yCordinate + otherCirclesRadius + 20} fill={otherCircleTextColor} stroke={otherCircleTextColor}>{title}</Text>
         </G>
       )
@@ -121,12 +121,12 @@ class NetworkGraph extends Component {
           {this.addBackgroundLines()}
           {this.addSelectedCircleLines()}
           <G>
-            {/* <Image
+            { <Image
               style={{ width: 60, height: 60, borderRadius: 60 / 2 }}
-              source={{ uri: 'https://exploringbits.com/wp-content/uploads/2022/01/cute-pfp-4.jpg?ezimgfmt=rs:352x354/rscb3/ng:webp/ngcb3' }} /> */}
-            {/* <Circle cx={containerWidth / 2} cy={containerHeight / 2} r={centralCircleRadius} stroke={centralCircleStrokeColor} strokeWidth="4" fill={centralCircleFillColor} style={{ borderWidth: 5 }} textAnchor="middle" />*/}
-            {/* <Circle cx={containerWidth / 2} cy={containerHeight / 2} r={centralCircleRadius} stroke={centralCircleStrokeColor} strokeWidth="4" fill="none" style={{ borderWidth: 5 }} textAnchor="middle" /> */}
-            <image xlinkHref={profiles["user"].imgurl} x={containerWidth / 2 - centralCircleRadius} y={containerHeight / 2 - centralCircleRadius} height={2 * centralCircleRadius} width={2 * centralCircleRadius} />
+              source={{ uri: 'https://exploringbits.com/wp-content/uploads/2022/01/cute-pfp-4.jpg?ezimgfmt=rs:352x354/rscb3/ng:webp/ngcb3' }} /> }
+            { <Circle cx={containerWidth / 2} cy={containerHeight / 2} r={centralCircleRadius} stroke={centralCircleStrokeColor} strokeWidth="4" fill={centralCircleFillColor} style={{ borderWidth: 5 }} textAnchor="middle" />}
+            { <Circle cx={containerWidth / 2} cy={containerHeight / 2} r={centralCircleRadius} stroke={centralCircleStrokeColor} strokeWidth="4" fill="none" style={{ borderWidth: 5 }} textAnchor="middle" /> }
+            {/*<Image xlinkHref={profiles["user"].imgurl} x={containerWidth / 2 - centralCircleRadius} y={containerHeight / 2 - centralCircleRadius} height={2 * centralCircleRadius} width={2 * centralCircleRadius} />*/}
             <Text x={containerWidth / 2 - centralCircleRadius + 10} y={containerHeight / 2 + centralCircleRadius + 15} fill={centralCircleTextColor} stroke={centralCircleTextColor}>{profiles["user"].uname}</Text>
           </G>
           {this.addCircles()}
